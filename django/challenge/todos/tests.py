5c468d7d2c2736f93545f52c9005afa64728ccd1
Diego Di Roberto <diroberto.diego@gmail.com>
2023-02-01 11:07:44 +0100

    Added TodoTestCase

--- django/challenge/todos/tests.py
+++ django/challenge/todos/tests.py
@@ -1,3 +1,18 @@
+import random
+from datetime import date, datetime
+
 from django.test import TestCase
 
-# Create your tests here.
+from todos.models import Todo
+
+
+class TodoTestCase(TestCase):
+    def create(self):
+        obj = Todo(
+            date=date.today(),
+            order=random.randint(0, 999),
+            description='Anim non officia elit aliqua aute aliqua minim',
+            created_at=datetime.now(),
+            updated_at=datetime.now(),
+        )
+        obj.save()
